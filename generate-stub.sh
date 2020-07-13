e1e5e84bcded84218e982183c00b164b7771078f
Edwin Betancourt <EdwinBetanc0urt@hotmail.com>
2020-07-13 12:26:31 -0400

    feat: Delete unused dependencies. (#9)

--- generate-stub.sh
+++ generate-stub.sh
@@ -1,4 +1,22 @@
+#####################################################################################
+# Product: ADempiere Generate Stub                                                  #
+# Copyright (C) 2012-2020 E.R.P. Consultores y Asociados, C.A.                      #
+# Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                      #
+# This program is free software: you can redistribute it and/or modify              #
+# it under the terms of the GNU General Public License as published by              #
+# the Free Software Foundation, either version 3 of the License, or                 #
+# (at your option) any later version.                                               #
+# This program is distributed in the hope that it will be useful,                   #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                    #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                     #
+# GNU General Public License for more details.                                      #
+# You should have received a copy of the GNU General Public License                 #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.            #
+#####################################################################################
+
+OUT_DIR=src/grpc
+
 # Generate Stub to enrollment.proto file
 protoc proto/enrollment.proto \
---js_out=import_style=commonjs:src/grpc \
---grpc-web_out=import_style=commonjs,mode=grpcwebtext:src/grpc
+    --js_out=import_style=commonjs:$OUT_DIR \
+    --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$OUT_DIR
